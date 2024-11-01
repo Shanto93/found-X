8528654b22034166f11cc38de4f0fc5b64753a97
Shanto93 <shantoislam7363@gmail.com>
2024-11-01 19:05:57 +0600

    Initial project setup for the client side and for the server side has been done

---
+++ foundx-client/src/app/(WithCommonLayout)/found-items/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FoundItems = () => {
+    return (
+        <div>
+            <h2>Found Items Page.</h2>
+            <h2>What's Happened.</h2>
+        </div>
+    );
+};
+
+export default FoundItems;
